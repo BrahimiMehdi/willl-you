8d9faa94c99503fd5bb1cb48ddcdd7994e64a949
BrahimiMehdi <brahimi.mehdi37@gmail.com>
2023-01-12 12:36:08 +0100

    changes

---
+++ components/Button.jsx
@@ -0,0 +1,38 @@
+import { useState } from "react";
+
+const Button = () => {
+  const [pos, setpos] = useState({ top: " top-8", left: "left-[35%] " });
+  const [count, setCount] = useState(0);
+  const randomPos = [
+    { top: " top-20", left: "left-20 " },
+    { top: " top-40", left: "left-60 " },
+    { top: " top-40", left: "left-[60%] " },
+    { top: " top-60", left: "left-40 " },
+    { top: " top-8", left: "left-4 " },
+    { top: " top-72", left: "left-12 " },
+    
+  ];
+  const changePos = () => {
+    if (count < randomPos.length -1) {
+      const newPos = randomPos[count];
+      setCount(count + 1);
+      setpos(newPos);
+    } else {
+      const newPos = randomPos[count];
+      setCount(0);
+      setpos(newPos);
+    }
+  };
+  return (
+    <button onClick={() => changePos()} className={`absolute flex justify-center group items-center transition-all duration-200 ease-out ${pos.top} ${pos.left}  bg-red-400`}>
+      <span className="relative left-4  group-hover:-translate-x-8 transition-transform duration-300 ease-out">
+        no
+      </span>
+      <span className="opacity-0 translate-x-8 transition-all duration-300 grid place-items-center group-hover:translate-x-0 group-hover:opacity-100">
+        <img className="w-8" src="/angry-face.png" alt="" />
+      </span>
+    </button>
+  );
+};
+
+export default Button;
--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,9 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import { Inter } from '@next/font/google'
-import styles from '../styles/Home.module.css'
+import Head from "next/head";
+import Image from "next/image";
+import { Button } from "../components";
+import { Moon_Dance } from "@next/font/google";
 
-const inter = Inter({ subsets: ['latin'] })
-
+const moon = Moon_Dance({ subsets: ["latin"], weight: "400" });
 export default function Home() {
   return (
     <>
@@ -14,110 +13,23 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <main className={styles.main}>
-        <div className={styles.description}>
-          <p>
-            Get started by editing&nbsp;
-            <code className={styles.code}>pages/index.js</code>
-          </p>
-          <div>
-            <a
-              href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              By{' '}
-              <Image
-                src="/vercel.svg"
-                alt="Vercel Logo"
-                className={styles.vercelLogo}
-                width={100}
-                height={24}
-                priority
-              />
-            </a>
+      <main className="h-screen grid grid-rows-2 gap-y-12 w-full bg-[#001118]">
+        <div className="w-full h-full grid grid-cols-3">
+          <div className="relative flex flex-col-reverse  col-start-2">
+            <h1 className={`text-4xl z-40 text-center ${moon.className} bg-[#001118] w-full text-white relative z-10 `}>will you date me?</h1>
+            <Image width={483} height={585} className="object-cover z-0 absolute top-0  h-full w-full object-top" src={"/catdate.webp"} />
           </div>
         </div>
-
-        <div className={styles.center}>
-          <Image
-            className={styles.logo}
-            src="/next.svg"
-            alt="Next.js Logo"
-            width={180}
-            height={37}
-            priority
-          />
-          <div className={styles.thirteen}>
-            <Image
-              src="/thirteen.svg"
-              alt="13"
-              width={40}
-              height={31}
-              priority
-            />
-          </div>
-        </div>
-
-        <div className={styles.grid}>
-          <a
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <h2 className={inter.className}>
-              Docs <span>-&gt;</span>
-            </h2>
-            <p className={inter.className}>
-              Find in-depth information about Next.js features and&nbsp;API.
-            </p>
-          </a>
-
-          <a
-            href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <h2 className={inter.className}>
-              Learn <span>-&gt;</span>
-            </h2>
-            <p className={inter.className}>
-              Learn about Next.js in an interactive course with&nbsp;quizzes!
-            </p>
-          </a>
-
-          <a
-            href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <h2 className={inter.className}>
-              Templates <span>-&gt;</span>
-            </h2>
-            <p className={inter.className}>
-              Discover and deploy boilerplate example Next.js&nbsp;projects.
-            </p>
-          </a>
-
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <h2 className={inter.className}>
-              Deploy <span>-&gt;</span>
-            </h2>
-            <p className={inter.className}>
-              Instantly deploy your Next.js site to a shareable URL
-              with&nbsp;Vercel.
-            </p>
-          </a>
+        <div className="relative flex justify-center gap-x-12">
+          <Button />
+          <button className="absolute flex justify-center group items-center top-8 left-[55%]">
+            <span className="relative left-4  group-hover:-translate-x-8 transition-transform duration-300 ease-out">yes</span>
+            <span className="opacity-0 translate-x-8 transition-all duration-300 grid place-items-center group-hover:translate-x-0 group-hover:opacity-100">
+              <img className="w-8" src="/cat.png" alt="" />
+            </span>
+          </button>
         </div>
       </main>
     </>
-  )
+  );
 }
